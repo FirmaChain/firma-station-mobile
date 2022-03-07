ca3c1583257ca3cd1caa1aa8101f178a3da72c9d
chill-beer <juno87210@gmail.com>
2022-03-07 22:46:27 +0900

    refactor: refactor transaction screen

---
+++ src/organims/transaction/index.tsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useState } from "react";
+import { Screens, StackParamList } from "@/navigators/appRoutes";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/native";
+import { useAppSelector } from "@/redux/hooks";
+import { BgColor } from "@/constants/theme";
+import { getWallet } from "@/util/wallet";
+import { TRANSACTION_TYPE } from "@/constants/common";
+import { delegate, redelegate, sendFCT, undelegate, withdrawAllRewards, withdrawRewards } from "@/util/firma";
+import ViewContainer from "@/components/parts/containers/viewContainer";
+import ProgressTransaction from "./progressTransaction";
+import TransactionResult from "./transactionResult";
+
+type ScreenNavgationProps = StackNavigationProp<StackParamList, Screens.Transaction>;
+
+interface Props {
+    state: any;
+}
+
+export interface ResultState {
+    code: number;
+    result: string;
+}
+
+const Transaction = ({state}:Props) => {
+    const navigation:ScreenNavgationProps = useNavigation();
+    const {wallet} = useAppSelector(state => state);
+
+    const [mnemonic, setMnemonic] = useState('');
+    const [transactionResult, setTransactionResult] = useState<ResultState>({
+        code: 0,
+        result: "",
+    });
+
+    useEffect(() => {
+        const getMnemonic = async() => {
+            await getWallet(wallet.name, state.password).then(res => {if(res) setMnemonic(res)});
+        }
+        getMnemonic();
+    }, []);
+
+    useEffect(() => {
+        if(mnemonic === '') return;
+        const transaction = async() => {
+            switch (state.type) {
+                case TRANSACTION_TYPE["SEND"]:
+                    await sendFCT(mnemonic, state.targetAddress, state.amount, state.gas, state.memo)
+                    .then(res => {
+                        setTransactionResult({
+                            code: res.code,
+                            result: res.transactionHash})})
+                    .catch(error => {
+                        console.log("ERROR : ", error.toString());
+                        setTransactionResult({
+                            code: -1,
+                            result: error.toString()})
+                    })
+                    break;
+                case TRANSACTION_TYPE["DELEGATE"]:
+                    await delegate(mnemonic, state.operatorAddressDst, state.amount, state.gas)
+                    .then(res => {
+                        setTransactionResult({
+                            code: res.code,
+                            result: res.transactionHash})})
+                    .catch(error => {
+                        console.log("ERROR : ", error.toString());
+                        setTransactionResult({
+                            code: -1,
+                            result: error.toString()})
+                    })
+                    break;
+                case TRANSACTION_TYPE["REDELEGATE"]:
+                    await redelegate(mnemonic, state.operatorAddressSrc, state.operatorAddressDst, state.amount, state.gas)
+                    .then(res => {
+                        setTransactionResult({
+                            code: res.code,
+                            result: res.transactionHash})})
+                    .catch(error => {
+                        console.log("ERROR : ", error.toString());
+                        setTransactionResult({
+                            code: -1,
+                            result: error.toString()})
+                    })
+                    break;
+                case TRANSACTION_TYPE["UNDELEGATE"]:
+                    await undelegate(mnemonic, state.operatorAddressDst, state.amount, state.gas)
+                    .then(res => {
+                        setTransactionResult({
+                            code: res.code,
+                            result: res.transactionHash})})
+                    .catch(error => {
+                        console.log("ERROR : ", error.toString());
+                        setTransactionResult({
+                            code: -1,
+                            result: error.toString()})
+                    })
+                    break;
+                case TRANSACTION_TYPE["WITHDRAW"]:
+                    await withdrawRewards(mnemonic, state.operatorAddress, state.gas)
+                    .then(res => {
+                        setTransactionResult({
+                            code: res.code,
+                            result: res.transactionHash})})
+                    .catch(error => {
+                        console.log("ERROR : ", error.toString());
+                        setTransactionResult({
+                            code: -1,
+                            result: error.toString()})
+                    })
+                    break;
+                case TRANSACTION_TYPE["WITHDRAW ALL"]:
+                    await withdrawAllRewards(mnemonic, state.gas)
+                    .then(res => {
+                        setTransactionResult({
+                            code: res.code,
+                            result: res.transactionHash})})
+                    .catch(error => {
+                        console.log("ERROR : ", error.toString());
+                        setTransactionResult({
+                            code: -1,
+                            result: error.toString()})
+                    })
+                    break;
+                default:
+                    break;
+            }
+        }
+        transaction();
+    }, [mnemonic])
+
+    const handleBack = () => {
+        navigation.goBack();
+    }
+
+    return (
+        <ViewContainer
+            full={true}
+            bgColor={BgColor}>
+                {transactionResult.result !== ""?
+                <TransactionResult result={transactionResult} handleBack={handleBack}/> 
+                :
+                <ProgressTransaction/>
+                }
+        </ViewContainer>
+    )
+}
+
+export default Transaction;
---
+++ src/organims/transaction/transactionResult.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Linking, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { EXPLORER } from "@/constants/common";
+import { BgColor, Lato, PointLightColor, TextColor, TextGrayColor, TextWarnColor } from "@/constants/theme";
+import { FailCircle, SuccessCircle } from "@/components/icon/icon";
+import Button from "@/components/button/button";
+import { ResultState } from ".";
+
+interface Props {
+    result: ResultState;
+    handleBack: () => void;
+}
+
+const TransactionResult = ({result, handleBack}:Props) => {
+
+    const convertTransactionCodeToText = (code:number) => {
+        if(code === 0) return "Transaction Success";
+        return "Transaction Failed";
+    }
+
+    const openExplorer = (hash:string) => {
+        if(result.code === -1) return;
+        Linking.openURL(EXPLORER + '/transactions/' + hash);
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <View style={[styles.resultBox, {flex: 1, justifyContent: "center", alignItems: "center"}]}>
+                {result.code === 0? 
+                <SuccessCircle size={45} color={PointLightColor}/>        
+                :
+                <FailCircle size={45} color={TextWarnColor}/>
+                }
+                <Text style={[styles.result, {color: result.code === 0? PointLightColor:TextWarnColor}]}>{convertTransactionCodeToText(result.code)}</Text>
+                <View style={styles.resultWrapper}>
+                    {result.code !== -1 && <Text style={[styles.hash, {color: TextGrayColor}]}>HASH: </Text>}
+                    <TouchableOpacity onPress={()=>openExplorer(result.result)}>
+                        <Text numberOfLines={result.code === -1?10:1} ellipsizeMode={"middle"} style={[styles.hash, {paddingHorizontal: 5}]}>{result.result}</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={[styles.resultBox, {justifyContent: "flex-end"}]}>
+            <Button
+                title={"OK"}
+                active={true}
+                onPressEvent={handleBack}/>
+            </View>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: BgColor,
+        paddingBottom: Platform.select({android: 30, ios: 50}),
+        paddingHorizontal: 20,
+    },
+    resultBox: {
+        width: "100%",
+    },
+    resultWrapper: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 20,
+    },
+    result: {
+        fontFamily: Lato,
+        fontSize: 22,
+        fontWeight: "600",
+        textAlign: "center",
+        color: PointLightColor,
+        paddingTop: 10,
+        paddingBottom: 20,
+    },
+    hash: {
+        fontFamily: Lato,
+        fontSize: 14,
+        textAlign: "center",
+        color: TextColor,
+        paddingBottom: 20,
+    }
+})
+
+export default TransactionResult;
