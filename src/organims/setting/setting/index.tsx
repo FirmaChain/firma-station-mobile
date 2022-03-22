b61bb19dd502f682e4f5d3dda4ac3a535e479396
chill-beer <juno87210@gmail.com>
2022-03-22 21:20:58 +0900

    update: wallet name added to setting screen

--- src/organims/setting/setting/index.tsx
+++ src/organims/setting/setting/index.tsx
@@ -4,7 +4,7 @@ import { Screens, StackParamList } from "@/navigators/appRoutes";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
 import { useAppSelector } from "@/redux/hooks";
-import { BgColor } from "@/constants/theme";
+import { AddressBoxColor, BgColor } from "@/constants/theme";
 import { removeWalletWithAutoLogin } from "@/util/wallet";
 import { VERSION } from "@/../config";
 import Container from "@/components/parts/containers/conatainer";
@@ -24,8 +24,8 @@ const Setting = () => {
     const settingList = [
         {title: 'Change Wallet name', path: 'ChangeWN'},
         {title: 'Change Password', path: 'ChangePW'},
-        {title: 'Export Private key', path: 'ExportPK'},
         {title: 'Export Mnemonic', path: 'ExportMN'},
+        {title: 'Export Private key', path: 'ExportPK'},
     ];
 
     const handleMenus = (path:string) => {
@@ -45,8 +45,8 @@ const Setting = () => {
         }
     }
 
-    const disconnectWallet = async() => {
-        await removeWalletWithAutoLogin();
+    const disconnectWallet = () => {
+        removeWalletWithAutoLogin();
         navigation.reset({routes: [{name: Screens.Welcome}]});
     }
 
@@ -59,29 +59,37 @@ const Setting = () => {
             title="Setting"
             backEvent={handleBack}>
             <ViewContainer bgColor={BgColor}>
-                <ScrollView 
-                    keyboardShouldPersistTaps={"handled"}
-                    style={{borderTopWidth: 1, borderTopColor: BgColor}}>
-                    <BioAuthRadio wallet={wallet}/>
-                    {settingList.map((item, index) => {
-                        return (
-                            <MenuItem key={index} title={item.title} path={item.path} handleMenus={handleMenus} />
-                        )
-                    })}
-                    <View style={styles.bottomButtonsBox}>
-                        <TextMenuItem title="Version" content={"v" + VERSION} />
-                    </View>
-                    <View style={styles.bottomButtonsBox}>
-                        <Disconnect handleDisconnect={disconnectWallet} />
-                        <Delete wallet={wallet} handleDisconnect={disconnectWallet} />
-                    </View>
-                </ScrollView>
+                <View style={{flex: 1}}>
+                    <ScrollView 
+                        keyboardShouldPersistTaps={"handled"}
+                        style={{borderTopWidth: 1, borderTopColor: BgColor}}>
+                        <View style={styles.topButtonsBox}>
+                            <TextMenuItem title="Wallet" content={wallet.name} bgColor={AddressBoxColor}/>
+                        </View>
+                        <BioAuthRadio wallet={wallet}/>
+                        {settingList.map((item, index) => {
+                            return (
+                                <MenuItem key={index} title={item.title} path={item.path} handleMenus={handleMenus} />
+                            )
+                        })}
+                        <View style={styles.bottomButtonsBox}>
+                            <TextMenuItem title="Version" content={"v" + VERSION} />
+                        </View>
+                        <View style={styles.bottomButtonsBox}>
+                            <Disconnect handleDisconnect={disconnectWallet} />
+                            <Delete wallet={wallet} handleDisconnect={disconnectWallet} />
+                        </View>
+                    </ScrollView>
+                </View>
             </ViewContainer>
         </Container>
     )
 }
 
 const styles = StyleSheet.create({
+    topButtonsBox: {
+        paddingBottom: 20,
+    },
     bottomButtonsBox: {
         paddingTop: 20,
     },
