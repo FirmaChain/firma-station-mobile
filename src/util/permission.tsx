30e344cd001fcba5828466e7f86a37c423826088
chill-beer <juno87210@gmail.com>
2022-04-20 15:09:33 +0900

    fix: changed the QR scanner permission verification method.

--- src/util/permission.tsx
+++ src/util/permission.tsx
@@ -1,18 +1,30 @@
 import { Alert, Linking, Platform } from "react-native";
-import { check, RESULTS, PERMISSIONS, Permission } from "react-native-permissions"
+import { check, RESULTS, PERMISSIONS, Permission, PermissionStatus, request } from "react-native-permissions"
 import { CAMERA_PERMISSION_ALERT } from "@/constants/common";
 
 export const isPermissionGranted = async(permission:Permission) => {
     let granted = false;
-    await check(permission)
-    .then((result) => {
-        if(RESULTS.DENIED === result || RESULTS.GRANTED === result){ granted = true;}
-        else {granted = false}
-    })
-    .catch((error) => {
-        console.log(error)
-        granted = false;
-    })
+    let requested: PermissionStatus;
+    const checked = await check(permission);
+    switch (checked) {
+        case RESULTS.GRANTED:
+            granted = true;
+            break;
+        case RESULTS.DENIED:
+            requested = await request(permission);
+            if (requested === RESULTS.GRANTED) {
+                granted = true;
+            }
+            break;
+        case RESULTS.UNAVAILABLE:   
+        case RESULTS.LIMITED:
+        case RESULTS.BLOCKED:
+            granted = false;
+            break;
+        default:
+            granted = false;
+            break;
+    }
 
     return granted;
 }
@@ -20,13 +32,11 @@ export const isPermissionGranted = async(permission:Permission) => {
 export const checkCameraPermission = async() => {
     let permission = false;
     if(Platform.OS === "ios"){
-        // permission = true;
         permission = await isPermissionGranted(PERMISSIONS.IOS.CAMERA);
     } else if(Platform.OS === "android") {
         permission = await isPermissionGranted(PERMISSIONS.ANDROID.CAMERA);
     }
     if(permission === false) {
-
         Alert.alert(CAMERA_PERMISSION_ALERT.title, CAMERA_PERMISSION_ALERT.desc, [
             {
                 text: "Cancel",
