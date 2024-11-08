7d5f462055cee6ae9fd1f0f1dfbc877ab75144b4
LarryKim <rlatkdgud98@naver.com>
2024-11-08 12:19:12 +0900

    refactor: remove rn-nodeify dependency

--- README.md
+++ README.md
@@ -1,49 +1,39 @@
 # Firma Station for Mobile
+
 <div style={{display: flex}}>
 <img height="400" src="https://user-images.githubusercontent.com/93243647/150078237-194c5fd9-ae78-4d3b-bf7c-9b7539758099.png">
 <img height="400" src="https://user-images.githubusercontent.com/93243647/150078250-e5220add-2af0-4a28-a6cb-fe52c98fa414.png">
 <img height="400" src="https://user-images.githubusercontent.com/93243647/150078252-cc64d88f-f33e-47c2-b933-7f48af9b6bc4.png">
 <div>
- 
-How to build and run firma-station-mobile
-=========================================
-### 1. install
+
+### 1. Requirements
+
+-   Node.JS: v16.20.2
+-   OpenJDK: 11.0.24
+
+### 2. install
+
+> **Do not remove .lock files for no reason.**
 
 1. Create a project with git clone.
-2. Replace the 'package.json' file with the 'origin-package.json' file.
-3. Install modules.
-<pre>
-  <code>
-    npm install
-  </code>
-</pre>
 
-4. Proceed in the following order.
-<pre>
-  <code>
-  cd ios
-  pod install
-  cd ..
+2. Install modules.
 
-  npm i --save-dev tradle/rn-nodeify
-  ./node_modules/.bin/rn-nodeify --hack --install
-  </code>
-</pre>
+```bash
+npm install
+```
 
-5. Add the following code to the shim.js file
-<pre>
-  <code>
-    ...
-    if (typeof BigInt === 'undefined') global.BigInt = require('big-integer');
-  </code>
-</pre>
+4. Install pod modules for iOS
 
-6. Imports shim.js to index.js.
+```bash
+cd ios
+pod install
+cd ..
+```
 
-### 2. Run
-<pre>
-  <code>
-  npm run ios
-  npm run android
-  </code>
-</pre>
+### 3. Run
+
+```bash
+npm run ios
+npm run android
+```
--- metro.config.js
+++ metro.config.js
@@ -2,16 +2,46 @@ const { getDefaultConfig } = require('metro-config');
 const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    ...defaultResolver,
-    sourceExts: [...defaultResolver.sourceExts, 'cjs'],
-  },
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true
+            }
+        })
+    },
+    resolver: {
+        ...defaultResolver,
+        sourceExts: [...defaultResolver.sourceExts, 'cjs'],
+        extraNodeModules: {
+            zlib: require.resolve('browserify-zlib'),
+            constants: require.resolve('constants-browserify'),
+            crypto: require.resolve('react-native-crypto'),
+            dns: require.resolve('dns.js'),
+            net: require.resolve('react-native-tcp-socket'),
+            domain: require.resolve('domain-browser'),
+            http: require.resolve('@tradle/react-native-http'),
+            https: require.resolve('@tradle/react-native-http'),
+            os: require.resolve('react-native-os'),
+            path: require.resolve('path-browserify'),
+            querystring: require.resolve('querystring-es3'),
+            fs: require.resolve('react-native-level-fs'),
+            _stream_transform: require.resolve('readable-stream/transform'),
+            _stream_readable: require.resolve('readable-stream/readable'),
+            _stream_writable: require.resolve('readable-stream/writable'),
+            _stream_duplex: require.resolve('readable-stream/duplex'),
+            _stream_passthrough: require.resolve('readable-stream/passthrough'),
+            dgram: require.resolve('react-native-udp'),
+            timers: require.resolve('timers-browserify'),
+            stream: require.resolve('stream-browserify'),
+            tty: require.resolve('tty-browserify'),
+            vm: require.resolve('vm-browserify'),
+            tls: false,
+            assert: require.resolve('assert'),
+            buffer: require.resolve('buffer'),
+            punycode: require.resolve('punycode'),
+            string_decoder: require.resolve('string_decoder'),
+            url: require.resolve('url')
+        }
+    }
 };
