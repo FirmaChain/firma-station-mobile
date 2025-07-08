aa95702c01bcc24a5e579b9dfdefe933cf62d757
LarryKim <rlatkdgud98@naver.com>
2025-07-08 18:08:27 +0900

    fix: set tx type color same with station desktop

--- src/constants/common.tsx
+++ src/constants/common.tsx
@@ -539,18 +539,15 @@ export const TRANSACTION_TYPE_MODEL: IKeyValue = {
         tagDisplay: LABELS.txCreateContractFileLabel,
     },
     '/cosmos.feegrant.v1beta1.MsgGrantAllowance': {
-        tagTheme: TYPE_COLORS.fourteen, // To match Explorer
-        // tagTheme: TYPE_COLORS.two,
+        tagTheme: TYPE_COLORS.two,
         tagDisplay: LABELS.txFeegrantGrantLabel,
     },
     '/cosmos.feegrant.v1beta1.MsgRevokeAllowance': {
-        tagTheme: TYPE_COLORS.fourteen, // To match Explorer
-        // tagTheme: TYPE_COLORS.three,
+        tagTheme: TYPE_COLORS.three,
         tagDisplay: LABELS.txFeegrantRevokeLabel,
     },
     '/cosmos.authz.v1beta1.MsgGrant': {
-        tagTheme: TYPE_COLORS.thirteen, // To match Explorer
-        // tagTheme: TYPE_COLORS.six,
+        tagTheme: TYPE_COLORS['two'], // To match Station desktop
         tagDisplay: LABELS.txAuthzGrantLabel,
     },
     '/firmachain.firmachain.token.MsgCreateToken': {
