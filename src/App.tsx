f8d4f262286ab9a4a68a7b2098b3ca2cf191efeb
LarryKim <rlatkdgud98@naver.com>
2025-08-11 10:12:44 +0900

    misc: hide redux selector warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Router from './navigators/router';
-import { StatusBar } from 'react-native';
+import { LogBox, StatusBar } from 'react-native';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+    LogBox.ignoreLogs(['Selector unknown returned the root state when called. This can lead to unnecessary rerenders.']);
+
     return (
         <SafeAreaProvider>
             <Provider store={store}>
