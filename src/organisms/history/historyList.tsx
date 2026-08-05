46a30d2c17bcc86707375dddee5e23efd0a2d5f7
Larrykim <larry@firmainnovation.com>
2026-08-05 17:25:16 +0900

    fix: app crash when entered send history page

--- src/organisms/history/historyList.tsx
+++ src/organisms/history/historyList.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { EXPLORER_URL } from '@/constants/common';
 import { BoxColor, Lato, TextCatTitleColor, TextColor, TextDarkGrayColor } from '@/constants/theme';
 import { convertTime } from '@/util/common';
@@ -16,8 +15,15 @@ interface IProps {
 }
 
 const HistoryList = ({ item, loading, handleExplorer }: IProps) => {
+    if (loading) {
+        return (
+            <View style={styles.skeleton}>
+                <RectangleSkeleton width={'100%'} height={'100%'} bgColor={BoxColor} />
+            </View>
+        );
+    }
+
     const inlineStyles1 = {
-        inlineStyle1: { paddingHorizontal: 20, width: '100%', height: 102, marginBottom: 10 },
         inlineStyle2: { paddingHorizontal: 20 },
         inlineStyle3: { flex: 2 },
         inlineStyle4: { alignItems: 'center', paddingBottom: 15 },
@@ -43,51 +49,46 @@ const HistoryList = ({ item, loading, handleExplorer }: IProps) => {
     } as const;
 
     return (
-        <Fragment>
-            {loading ? (
-                <View style={inlineStyles1.inlineStyle1}>
-                    <RectangleSkeleton width={'100%'} height={'100%'} bgColor={BoxColor} />
-                </View>
-            ) : (
-                <TouchableOpacity
-                    style={inlineStyles1.inlineStyle2}
-                    onPress={() => handleExplorer(EXPLORER_URL() + '/transactions/' + item.hash)}
-                >
-                    <View style={styles.box}>
-                        <View style={styles.wrapperH}>
-                            <View style={inlineStyles1.inlineStyle3}>
-                                <View style={[styles.wrapperH, inlineStyles1.inlineStyle4]}>
-                                    <Text style={[styles.contentTitle, inlineStyles1.inlineStyle5]}>
-                                        {convertTime(item.timestamp, true, false)}
-                                    </Text>
-                                </View>
-                                <View style={[styles.wrapperH, styles.wrapper, inlineStyles1.inlineStyle6]}>
-                                    <View style={[styles.historyWrapper, inlineStyles1.inlineStyle7]}>
-                                        <Text style={[styles.contentTitle, inlineStyles1.inlineStyle8]}>Block</Text>
-                                        <Text style={[styles.contentItem, inlineStyles1.inlineStyle9]}>{item.block}</Text>
-                                    </View>
-                                    <View style={[styles.historyWrapper, inlineStyles1.inlineStyle10]}>
-                                        <Text style={[styles.contentTitle, inlineStyles1.inlineStyle11]}>Type</Text>
-                                        <Text style={[styles.contentItem, inlineStyles1.inlineStyle12]}>{item.type.tagDisplay}</Text>
-                                    </View>
-                                    <View style={[styles.historyWrapper, inlineStyles1.inlineStyle13]}>
-                                        <Text style={[styles.contentTitle, inlineStyles1.inlineStyle14]}>Result</Text>
-                                        <Text style={[styles.contentItem, inlineStyles1.inlineStyle15]}>{item.success}</Text>
-                                    </View>
-                                </View>
+        <TouchableOpacity style={inlineStyles1.inlineStyle2} onPress={() => handleExplorer(EXPLORER_URL() + '/transactions/' + item.hash)}>
+            <View style={styles.box}>
+                <View style={styles.wrapperH}>
+                    <View style={inlineStyles1.inlineStyle3}>
+                        <View style={[styles.wrapperH, inlineStyles1.inlineStyle4]}>
+                            <Text style={[styles.contentTitle, inlineStyles1.inlineStyle5]}>
+                                {convertTime(item.timestamp, true, false)}
+                            </Text>
+                        </View>
+                        <View style={[styles.wrapperH, styles.wrapper, inlineStyles1.inlineStyle6]}>
+                            <View style={[styles.historyWrapper, inlineStyles1.inlineStyle7]}>
+                                <Text style={[styles.contentTitle, inlineStyles1.inlineStyle8]}>Block</Text>
+                                <Text style={[styles.contentItem, inlineStyles1.inlineStyle9]}>{item.block}</Text>
                             </View>
-                            <View style={inlineStyles1.inlineStyle16}>
-                                <ForwardArrow size={20} color={TextCatTitleColor} />
+                            <View style={[styles.historyWrapper, inlineStyles1.inlineStyle10]}>
+                                <Text style={[styles.contentTitle, inlineStyles1.inlineStyle11]}>Type</Text>
+                                <Text style={[styles.contentItem, inlineStyles1.inlineStyle12]}>{item.type.tagDisplay}</Text>
+                            </View>
+                            <View style={[styles.historyWrapper, inlineStyles1.inlineStyle13]}>
+                                <Text style={[styles.contentTitle, inlineStyles1.inlineStyle14]}>Result</Text>
+                                <Text style={[styles.contentItem, inlineStyles1.inlineStyle15]}>{item.success}</Text>
                             </View>
                         </View>
                     </View>
-                </TouchableOpacity>
-            )}
-        </Fragment>
+                    <View style={inlineStyles1.inlineStyle16}>
+                        <ForwardArrow size={20} color={TextCatTitleColor} />
+                    </View>
+                </View>
+            </View>
+        </TouchableOpacity>
     );
 };
 
 const styles = StyleSheet.create({
+    skeleton: {
+        paddingHorizontal: 20,
+        width: '100%',
+        height: 102,
+        marginBottom: 10
+    },
     historyWrapper: {
         flex: 1,
         justifyContent: 'space-between',
