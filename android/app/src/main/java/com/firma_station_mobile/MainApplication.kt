c13d462654bf08823ed8c11536972d73af8c9163
Larrykim <larry@firmainnovation.com>
2026-08-06 17:36:20 +0900

    fix: apply correct font weights on Android
    - Replace Lato fonts with the latest version
    - Add Android font resource mappings for each weight
    - Register font families to ensure correct font weights are applied on Android

--- android/app/src/main/java/com/firma_station_mobile/MainApplication.kt
+++ android/app/src/main/java/com/firma_station_mobile/MainApplication.kt
@@ -5,6 +5,7 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.common.assets.ReactFontManager
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 
 class MainApplication : Application(), ReactApplication {
@@ -22,6 +23,8 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+    ReactFontManager.getInstance().addCustomFont(this, "Lato-Regular", R.font.lato)
+    ReactFontManager.getInstance().addCustomFont(this, "ChakraPetch-Regular", R.font.chakra_petch)
     loadReactNative(this)
   }
 }
